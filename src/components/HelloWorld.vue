68c8a5f0c78681e501c7e89ea7cc437d8a16f7aa
dwightjack <marco.solazzi@gmail.com>
2024-01-12 11:33:13 +0900

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import VueTypes from 'vue-types';
+
+const props = defineProps({
+  params: VueTypes.object.def({}),
+});
+</script>
+
+<template>
+  <p>{{ params }}</p>
+</template>
